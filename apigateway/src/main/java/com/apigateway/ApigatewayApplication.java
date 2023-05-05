be723079494a8ddc897b56fe5709f8f16e714d04
engrabdulrauf4u <engrabdulrauf4u@gmail.com>
2023-05-05 15:10:31 +0500

    prefer iP Address true

--- apigateway/src/main/java/com/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/apigateway/ApigatewayApplication.java
@@ -1,16 +1,28 @@
 package com.apigateway;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
+
+	/*	HttpClient httpClient = HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+		WebClient dd = WebClient.builder()
+				.clientConnector(new ReactorClientHttpConnector(httpClient))
+				.build();
+		System.out.println("Response " + dd);*/
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
 
